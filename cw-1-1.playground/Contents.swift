1786231943e8b8a3b7780aa0ac6df4372d2398fa
fatmahsaleemah <66628606+fatmahsaleemah@users.noreply.github.com>
2020-09-09 20:38:17 +0300

    done

---
+++ cw-1-1.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let name = "ساره"
+var age = 14
+var height = 1.55
+var hoppy1 = "البرمجه"
+var hoppy2 = "الرسم"
+print("السلام عليكم،اسمي \(name) انا احب \(hoppy1) و \(hoppy2). عمري \(age) وطولي \(height)")
+let friendName = "نوره"
+var friendHeight = 1.6
+var friendAge = 15
+var friendHobby1 = "الرسم"
+var friendHobby2 = "السباحه"
+
+var age_difference = friendAge - age
+print (age_difference)
+friendAge > age
+friendAge < age
+height == friendHeight
+height < friendHeight
+height > friendHeight
+height == friendHeight
+hoppy1 == friendHobby1 || hoppy1 == friendHobby2 || hoppy2 == friendHobby1 || hoppy2 == friendHobby2
